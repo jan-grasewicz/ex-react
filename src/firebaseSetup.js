20dc7d0e0ccc0ec6656d75ee65c4332da1bdc371
Jan Grasewicz <j.grasewicz@gmail.com>
2019-03-03 10:41:36 +0100

    firebase added

---
+++ src/firebaseSetup.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBl111hIYKHhep5MQXJUAlRHa9_rwDEjMQ",
+  authDomain: "project-02-25.firebaseapp.com",
+  databaseURL: "https://project-02-25.firebaseio.com",
+  projectId: "project-02-25",
+  storageBucket: "project-02-25.appspot.com",
+  messagingSenderId: "898841356191"
+};
+firebase.initializeApp(config);
